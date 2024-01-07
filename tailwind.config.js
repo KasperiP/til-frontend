4f0b22e37ab8f7a561ba36db1e9db02867b368a8
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-08 00:45:18 +0200

    add login buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: "jit",
-  content: ["./src/**/*.{html,ts}"],
-  darkMode: "class",
+  mode: 'jit',
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: 'class',
   theme: {
     extend: {
       screens: {
-        xs: "375px",
+        xs: '375px',
       },
       borderWidth: {
-        1: "1px",
+        1: '1px',
       },
       colors: {
-        divider: "var(--divider)",
+        divider: 'var(--divider)',
+        typography: {
+          primary: 'var(--typography-primary)',
+        },
       },
     },
   },
@@ -20,30 +23,26 @@ module.exports = {
     themes: [
       {
         dark: {
-          ...require("daisyui/src/theming/themes")["dark"],
-          "--divider": "#f3f3f3",
-          "neutral-content": "#f3f3f3",
-          "base-100": "#fff",
-          neutral: "#fff",
-          ".typography-primary": {
-            color: "#202125",
-          },
+          ...require('daisyui/src/theming/themes')['dark'],
+          '--divider': '#f3f3f3',
+          'neutral-content': '#f3f3f3',
+          'base-100': '#fff',
+          neutral: '#fff',
+          '--typography-primary': '#202125',
         },
       },
       {
         light: {
-          ...require("daisyui/src/theming/themes")["light"],
-          primary: "#f7f0cd",
-          "--divider": "#f3f3f3",
-          "neutral-content": "#f3f3f3",
-          "base-100": "#fff",
-          neutral: "#f5f5f5",
-          ".typography-primary": {
-            color: "#202125",
-          },
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#f7f0cd',
+          '--divider': '#f3f3f3',
+          'neutral-content': '#f3f3f3',
+          'base-100': '#fff',
+          neutral: '#f5f5f5',
+          '--typography-primary': '#202125',
         },
       },
     ],
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
