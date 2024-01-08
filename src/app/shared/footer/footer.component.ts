cedcb1ff5868281aa99f49f5a775a696481aa9bf
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-08 19:22:19 +0200

    add footer design

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  get currentYear(): number {
+    return new Date().getFullYear();
+  }
+}
