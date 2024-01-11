0e7d28be002a1ac5446337ea58abcaa12e0a26f8
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-11 16:41:45 +0200

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,11 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://today-i-learned-backend.vercel.app',
+  baseUrl: 'https://api.til.kassq.dev',
   googleClientId:
     '546562099406-n7ureg9ebpr7kcc7lgpspcai3741mufj.apps.googleusercontent.com',
-  googleRedirectUri:
-    'https://today-i-learned-frontend.vercel.app/auth/oauth/google/callback',
+  googleRedirectUri: 'https://til.kassq.dev/auth/oauth/google/callback',
   githubClientId: '4f3382afbcf769a54b61',
-  githubRedirectUri:
-    'https://today-i-learned-frontend.vercel.app/auth/oauth/github/callback',
+  githubRedirectUri: 'https://til.kassq.dev/auth/oauth/github/callback',
   linkedinClientId: '77mc358dc7r7to',
-  linkedinRedirectUri:
-    'https://today-i-learned-frontend.vercel.app/auth/oauth/linkedin/callback',
+  linkedinRedirectUri: 'https://til.kassq.dev/auth/oauth/linkedin/callback',
 };
