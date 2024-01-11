3f2e5274c6ed462ef52c151c03fe39ac0292cce5
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-11 17:26:28 +0200

    add credentials interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,21 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideQuillConfig } from 'ngx-quill';
 import { routes } from './app.routes';
+import { credentialsInterceptor } from './core/interceptors/credentials.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([credentialsInterceptor])),
     importProvidersFrom(BrowserAnimationsModule),
     provideQuillConfig({
       modules: {
