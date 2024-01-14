82b6ca91d93cb684eb3edead23064db5c62a1049
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-14 17:39:54 +0200

    scroll to top on page change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  OnDestroy,
+  OnInit,
+  PLATFORM_ID,
+} from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Subject, filter, takeUntil } from 'rxjs';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
@@ -11,4 +19,27 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly onDestroy$ = new Subject<void>();
+
+  constructor(
+    @Inject(PLATFORM_ID) private readonly platformId: string,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.router.events
+        .pipe(
+          filter((event) => event instanceof NavigationEnd),
+          takeUntil(this.onDestroy$),
+        )
+        .subscribe(() => window.scrollTo(0, 0));
+    }
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+}
