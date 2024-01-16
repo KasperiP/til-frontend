898fec7a20733c3f56c1794ad8b0e62d28c74701
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-16 13:45:32 +0200

    add missing links

--- src/app/pages/home/components/faq/faq.component.ts
+++ src/app/pages/home/components/faq/faq.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'til-faq',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
