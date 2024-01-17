3fae97f4ff20ed941866f94c07c6a534d39e3a2f
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2024-01-17 17:54:51 +0200

    add initial profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'til-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  readonly usernameControl: FormControl;
+  readonly user$ = this.userService.user$;
+
+  constructor(private readonly userService: UserService) {
+    this.usernameControl = new FormControl('', {
+      validators: [Validators.minLength(3), Validators.maxLength(30)],
+    });
+  }
+
+  changeUsername() {
+    // TODO: implement
+  }
+}
